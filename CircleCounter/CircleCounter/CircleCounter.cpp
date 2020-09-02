197c65a9ee213348e24dfe7bfdf8e84314f0a1ce
Aleksej Kostrykin <aleksejkostrykin@icloud.com>
2020-09-02 20:44:08 +0300

    Two Type

--- CircleCounter/CircleCounter/CircleCounter.cpp
+++ CircleCounter/CircleCounter/CircleCounter.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include "Circle.cpp"
 
-int main()
+void GetObjectProgrammingAlghorytm()
 {
+
+    std::cout << "OBJECT Programming" << std::endl;
+
     double radius;
     double arcAngle;
 
     Circle newCircle;
+
     std::cout << "Enter Radius: ";
     std::cin >> radius;
 
@@ -20,5 +24,28 @@ int main()
     std::cout << "Square = " << newCircle.GetSquare(arcAngle) << std::endl;
     std::cout << "Arc Length = " << newCircle.GetSquare(arcAngle) << std::endl;
 
-    return 0;
+    return;
+};
+
+void GetProceduralProgrammingAlghorytm()
+{
+    std::cout << "Procedural Programming" << std::endl;
+
+    double radius, arcAngle;
+
+    std::cout << "Enter Radius: ";
+    std::cin >> radius;
+
+    std::cout << "Enter Angle: ";
+    std::cin >> arcAngle;
+
+    std::cout << "Square = " << (((M_PI * radius) / 180) * arcAngle) << std::endl;
+    std::cout << "Arc Length = " << (M_PI * pow(radius, 2) * arcAngle) / 360 << std::endl;
+
+    return;
+};
+
+int main()
+{
+    GetObjectProgrammingAlghorytm();
 }
